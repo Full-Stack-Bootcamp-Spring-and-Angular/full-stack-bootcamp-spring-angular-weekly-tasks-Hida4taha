b3e08a63cd4f59dd075f4ba0f4e807d9fdef33ce
HODA <hodaelzyady96@gmail.com>
2025-12-29 22:09:17 +0200

    task3 is solved

---
+++ Task3/src/com/Draw2D.java
@@ -0,0 +1,7 @@
+package com;
+
+public class Draw2D {
+public void draw2D(){
+    System.out.println("shape is Drawing 2D");
+}
+}
---
+++ Task3/src/com/Square.java
@@ -0,0 +1,24 @@
+package com;
+
+public class Square implements Shape{
+    private Draw2D draw2D;
+    private  Draw3D draw3D;
+
+    public void setDraw2D(Draw2D draw2D) {
+        this.draw2D = draw2D;
+    }
+
+    public void setDraw3D(Draw3D draw3D) {
+        this.draw3D = draw3D;
+    }
+
+    @Override
+    public void Draw2D() {
+draw2D.draw2D();
+    }
+
+    @Override
+    public void Draw3D() {
+draw3D.draw3D();
+    }
+}
